f0e09b600e039f6e052bf05d6c93ac8bffc89276
Harper Frankstone <Harper Frankstone>
2017-04-07 13:48:53 -0500

    nickelback...not up in here!

---
+++ nickelback.py
@@ -0,0 +1,13 @@
+songs = {
+    ('Nickelback', 'How You Remind Me'), 
+    ('Will.i.am', 'That Power'),
+    ('Miles Davis', 'Stella by Starlight'),
+    ('Nickelback', 'Animals')
+}
+
+for song in songs: 
+	if song[0] != 'Nickelback':
+		no_nickelback_here = song
+		print(no_nickelback_here)
+
+# print("{} sings {}".format())
